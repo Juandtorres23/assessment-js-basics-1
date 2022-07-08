a207a5f36b8352de31986d729f3c20f5ce3b3924
Juan Torres Reyes <juandtorres23@yahoo.com>
2022-07-08 14:03:59 -0400

    Problem 3 solved

--- arrays.js
+++ arrays.js
@@ -1,17 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+// const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
-var colorCopy = faveColors.slice(0, 3)
-console.log(colorCopy)
+// var colorCopy = faveColors.slice(0, 3)
+// console.log(colorCopy)
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-faveColors.push('blue')
-console.log(faveColors)
+// faveColors.push('blue')
+// console.log(faveColors)
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -21,7 +21,8 @@ console.log(faveColors)
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+var middleNums = numbers.slice(1, 4)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
